d9279d4f92e00b097db96ef35422f81a188e2c78
Paul <peiche@gmail.com>
2023-01-14 20:54:11 -0600

    Formatting

--- inc/block-styles.php
+++ inc/block-styles.php
@@ -6,83 +6,83 @@
  */
 
 add_action( 'init', function() {
-    register_block_style( 'core/button', [
-        'name' => 'underline',
-        'label' => __( 'Underline', 'ankur' ),
-        'attributes' => [
-            'className' => 'is-style-underline',
-        ],
-    ] );
+	register_block_style( 'core/button', [
+		'name' => 'underline',
+		'label' => __( 'Underline', 'ankur' ),
+		'attributes' => [
+			'className' => 'is-style-underline',
+		],
+	] );
 
-    register_block_style( 'core/query-pagination', [
-        'name' => 'buttons',
-        'label' => __( 'Buttons', 'ankur' ),
-        'attributes' => [
-            'className' => 'is-style-buttons',
-        ],
-    ] );
+	register_block_style( 'core/query-pagination', [
+		'name' => 'buttons',
+		'label' => __( 'Buttons', 'ankur' ),
+		'attributes' => [
+			'className' => 'is-style-buttons',
+		],
+	] );
 
-    register_block_style( 'core/post-terms', [
-        'name' => 'buttons',
-        'label' => __( 'Buttons', 'ankur' ),
-        'attributes' => [
-            'className' => 'is-style-buttons',
-        ],
-    ] );
+	register_block_style( 'core/post-terms', [
+		'name' => 'buttons',
+		'label' => __( 'Buttons', 'ankur' ),
+		'attributes' => [
+			'className' => 'is-style-buttons',
+		],
+	] );
 
-    register_block_style( 'core/post-terms', [
-        'name' => 'outline',
-        'label' => __( 'Outline', 'ankur' ),
-        'attributes' => [
-            'className' => 'is-style-outline',
-        ],
-    ] );
+	register_block_style( 'core/post-terms', [
+		'name' => 'outline',
+		'label' => __( 'Outline', 'ankur' ),
+		'attributes' => [
+			'className' => 'is-style-outline',
+		],
+	] );
 
-    register_block_style( 'core/quote', [
-        'name' => 'large',
-        'label' => __( 'Large', 'ankur' ),
-        'attributes' => [
-            'className' => 'is-style-large',
-        ],
-    ] );
+	register_block_style( 'core/quote', [
+		'name' => 'large',
+		'label' => __( 'Large', 'ankur' ),
+		'attributes' => [
+			'className' => 'is-style-large',
+		],
+	] );
 
-    register_block_style( 'core/image', [
-        'name' => 'pill',
-        'label' => __( 'Pill', 'ankur' ),
-        'attributes' => [
-            'className' => 'is-style-pill',
-        ],
-    ] );
+	register_block_style( 'core/image', [
+		'name' => 'pill',
+		'label' => __( 'Pill', 'ankur' ),
+		'attributes' => [
+			'className' => 'is-style-pill',
+		],
+	] );
 
-    register_block_style( 'core/embed', [
-        'name' => 'rounded',
-        'label' => __( 'Rounded', 'ankur' ),
-        'attributes' => [
-            'className' => 'is-style-rounded',
-        ],
-    ] );
+	register_block_style( 'core/embed', [
+		'name' => 'rounded',
+		'label' => __( 'Rounded', 'ankur' ),
+		'attributes' => [
+			'className' => 'is-style-rounded',
+		],
+	] );
 
-    register_block_style( 'core/gallery', [
-        'name' => 'diamond',
-        'label' => __( 'Diamond', 'ankur' ),
-        'attributes' => [
-            'className' => 'is-style-diamond',
-        ],
-    ] );
+	register_block_style( 'core/gallery', [
+		'name' => 'diamond',
+		'label' => __( 'Diamond', 'ankur' ),
+		'attributes' => [
+			'className' => 'is-style-diamond',
+		],
+	] );
 
-    register_block_style( 'core/media-text', [
-        'name' => 'overlap',
-        'label' => __( 'Text Overlap', 'ankur' ),
-        'attributes' => [
-            'className' => 'is-style-overlap',
-        ],
-    ] );
+	register_block_style( 'core/media-text', [
+		'name' => 'overlap',
+		'label' => __( 'Text Overlap', 'ankur' ),
+		'attributes' => [
+			'className' => 'is-style-overlap',
+		],
+	] );
 
-    register_block_style( 'core/cover', [
-        'name' => 'rounded',
-        'label' => __( 'Rounded', 'ankur' ),
-        'attributes' => [
-            'className' => 'is-style-rounded',
-        ],
-    ] );
+	register_block_style( 'core/cover', [
+		'name' => 'rounded',
+		'label' => __( 'Rounded', 'ankur' ),
+		'attributes' => [
+			'className' => 'is-style-rounded',
+		],
+	] );
 } );
