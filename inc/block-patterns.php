264c4266339378e6585c4cc884592389efbfaf7f
Paul <peiche@gmail.com>
2023-10-30 17:12:06 -0500

    Moved anonymous functions to named functions.

--- inc/block-patterns.php
+++ inc/block-patterns.php
@@ -6,24 +6,25 @@
  * @since Ankur 1.0
  */
 
-add_action(
-	'init',
-	function () {
-		$block_pattern_categories = array(
-			'author' => array( 'label' => __( 'Author', 'ankur' ) ),
-			'banner' => array( 'label' => __( 'Banner', 'ankur' ) ),
-			'card'   => array( 'label' => __( 'Card', 'ankur' ) ),
-			'header' => array( 'label' => __( 'Header', 'ankur' ) ),
-			'hero'   => array( 'label' => __( 'Hero', 'ankur' ) ),
-			'quote'  => array( 'label' => __( 'Quote', 'ankur' ) ),
-		);
+/**
+ * Register custom block patterns.
+ */
+function ankur_register_block_patterns() {
+	$block_pattern_categories = array(
+		'author' => array( 'label' => __( 'Author', 'ankur' ) ),
+		'banner' => array( 'label' => __( 'Banner', 'ankur' ) ),
+		'card'   => array( 'label' => __( 'Card', 'ankur' ) ),
+		'header' => array( 'label' => __( 'Header', 'ankur' ) ),
+		'hero'   => array( 'label' => __( 'Hero', 'ankur' ) ),
+		'quote'  => array( 'label' => __( 'Quote', 'ankur' ) ),
+	);
 
-		foreach ( $block_pattern_categories as $name => $properties ) {
+	foreach ( $block_pattern_categories as $name => $properties ) {
 
-			if ( ! WP_Block_Pattern_Categories_Registry::get_instance()->is_registered( $name ) ) {
-				register_block_pattern_category( $name, $properties );
-			}
+		if ( ! WP_Block_Pattern_Categories_Registry::get_instance()->is_registered( $name ) ) {
+			register_block_pattern_category( $name, $properties );
 		}
-	},
-	9
-);
+	}
+}
+
+add_action( 'init', 'ankur_register_block_patterns', 9 );
--- inc/block-styles.php
+++ inc/block-styles.php
@@ -6,128 +6,130 @@
  * @since Ankur 1.0
  */
 
-add_action(
-	'init',
-	function () {
-		register_block_style(
-			'core/button',
-			array(
-				'name'       => 'underline',
-				'label'      => __( 'Underline', 'ankur' ),
-				'attributes' => array(
-					'className' => 'is-style-underline',
-				),
-			)
-		);
+/**
+ * Register custom block styles.
+ */
+function ankur_register_block_styles() {
+	register_block_style(
+		'core/button',
+		array(
+			'name'       => 'underline',
+			'label'      => __( 'Underline', 'ankur' ),
+			'attributes' => array(
+				'className' => 'is-style-underline',
+			),
+		)
+	);
 
-		register_block_style(
-			'core/query-pagination',
-			array(
-				'name'       => 'buttons',
-				'label'      => __( 'Buttons', 'ankur' ),
-				'attributes' => array(
-					'className' => 'is-style-buttons',
-				),
-			)
-		);
+	register_block_style(
+		'core/query-pagination',
+		array(
+			'name'       => 'buttons',
+			'label'      => __( 'Buttons', 'ankur' ),
+			'attributes' => array(
+				'className' => 'is-style-buttons',
+			),
+		)
+	);
 
-		register_block_style(
-			'core/post-terms',
-			array(
-				'name'       => 'buttons',
-				'label'      => __( 'Buttons', 'ankur' ),
-				'attributes' => array(
-					'className' => 'is-style-buttons',
-				),
-			)
-		);
+	register_block_style(
+		'core/post-terms',
+		array(
+			'name'       => 'buttons',
+			'label'      => __( 'Buttons', 'ankur' ),
+			'attributes' => array(
+				'className' => 'is-style-buttons',
+			),
+		)
+	);
 
-		register_block_style(
-			'core/post-terms',
-			array(
-				'name'       => 'outline',
-				'label'      => __( 'Outline', 'ankur' ),
-				'attributes' => array(
-					'className' => 'is-style-outline',
-				),
-			)
-		);
+	register_block_style(
+		'core/post-terms',
+		array(
+			'name'       => 'outline',
+			'label'      => __( 'Outline', 'ankur' ),
+			'attributes' => array(
+				'className' => 'is-style-outline',
+			),
+		)
+	);
 
-		register_block_style(
-			'core/quote',
-			array(
-				'name'       => 'large',
-				'label'      => __( 'Large', 'ankur' ),
-				'attributes' => array(
-					'className' => 'is-style-large',
-				),
-			)
-		);
+	register_block_style(
+		'core/quote',
+		array(
+			'name'       => 'large',
+			'label'      => __( 'Large', 'ankur' ),
+			'attributes' => array(
+				'className' => 'is-style-large',
+			),
+		)
+	);
 
-		register_block_style(
-			'core/image',
-			array(
-				'name'       => 'pill',
-				'label'      => __( 'Pill', 'ankur' ),
-				'attributes' => array(
-					'className' => 'is-style-pill',
-				),
-			)
-		);
+	register_block_style(
+		'core/image',
+		array(
+			'name'       => 'pill',
+			'label'      => __( 'Pill', 'ankur' ),
+			'attributes' => array(
+				'className' => 'is-style-pill',
+			),
+		)
+	);
 
-		register_block_style(
-			'core/embed',
-			array(
-				'name'       => 'rounded',
-				'label'      => __( 'Rounded', 'ankur' ),
-				'attributes' => array(
-					'className' => 'is-style-rounded',
-				),
-			)
-		);
+	register_block_style(
+		'core/embed',
+		array(
+			'name'       => 'rounded',
+			'label'      => __( 'Rounded', 'ankur' ),
+			'attributes' => array(
+				'className' => 'is-style-rounded',
+			),
+		)
+	);
 
-		register_block_style(
-			'core/gallery',
-			array(
-				'name'       => 'diamond',
-				'label'      => __( 'Diamond', 'ankur' ),
-				'attributes' => array(
-					'className' => 'is-style-diamond',
-				),
-			)
-		);
+	register_block_style(
+		'core/gallery',
+		array(
+			'name'       => 'diamond',
+			'label'      => __( 'Diamond', 'ankur' ),
+			'attributes' => array(
+				'className' => 'is-style-diamond',
+			),
+		)
+	);
 
-		register_block_style(
-			'core/media-text',
-			array(
-				'name'       => 'overlap',
-				'label'      => __( 'Text Overlap', 'ankur' ),
-				'attributes' => array(
-					'className' => 'is-style-overlap',
-				),
-			)
-		);
+	register_block_style(
+		'core/media-text',
+		array(
+			'name'       => 'overlap',
+			'label'      => __( 'Text Overlap', 'ankur' ),
+			'attributes' => array(
+				'className' => 'is-style-overlap',
+			),
+		)
+	);
 
-		register_block_style(
-			'core/cover',
-			array(
-				'name'       => 'rounded',
-				'label'      => __( 'Rounded', 'ankur' ),
-				'attributes' => array(
-					'className' => 'is-style-rounded',
-				),
-			)
-		);
+	register_block_style(
+		'core/cover',
+		array(
+			'name'       => 'rounded',
+			'label'      => __( 'Rounded', 'ankur' ),
+			'attributes' => array(
+				'className' => 'is-style-rounded',
+			),
+		)
+	);
 
-		register_block_style(
-			'core/video',
-			array(
-				'name'       => 'rounded',
-				'label'      => __( 'Rounded', 'ankur' ),
-				'attributes' => array(
-					'className' => 'is-style-rounded',
-				),
-			)
-		);
-	}
-);
+	register_block_style(
+		'core/video',
+		array(
+			'name'       => 'rounded',
+			'label'      => __( 'Rounded', 'ankur' ),
+			'attributes' => array(
+				'className' => 'is-style-rounded',
+			),
+		)
+	);
+}
+
+add_action( 'init', 'ankur_register_block_styles' );
